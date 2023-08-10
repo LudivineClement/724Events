9a0bd9de19b4b9e32584ef4ca5ac0748b73f0dfa
ludivineclement <ludivine.clement0985@gmail.com>
2023-08-10 18:33:19 +0200

    finalisation projet

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,8 +8,8 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  ).reverse();
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
+  );
 
   const imgSlide = data?.focus?.length;
 
